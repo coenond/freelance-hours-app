56a0de9abb46d7b9674135b914f3468b50d5075c
Coen Onderwater <coenonderwater@gmail.com>
2018-12-18 09:26:30 +0100

    init android project

---
+++ app/src/main/java/com/coen/freelancehours/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.coen.freelancehours
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
